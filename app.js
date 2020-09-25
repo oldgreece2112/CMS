069b8e4cc26e9c29218b0a12091083dbac490b05
Alexander Sofianidis <ansofianidis@outlook.com>
2020-09-25 21:09:38 +0300

    added base configurations for app.js

--- app.js
+++ app.js
@@ -11,8 +11,30 @@ var passportLocal = require("passport-local");
 var session = require("express-session");
 var app = express();
 
-var PORT = provess.env.PORT || 8000;
+var User = require("./models/user.model");
+var PORT = process.env.PORT || 8000;
 
-app.listen(PORt, process.env.IP, function() {
-    console.log("The server is currently listening on port " + PORt);
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Connected to database");
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.use(new passportLocal(User.authenticate()));
+
+app.listen(PORT, process.env.IP, function() {
+    console.log("The server is currently listening on port " + PORT);
 });
